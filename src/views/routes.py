3279bc6edda3624ccd5e4fb79ae503a6b26800e1
loverofMush <tolujamal1@gmail.com>
2021-02-12 02:42:14 +0100

    - fetch new indicator values from cache

--- src/views/routes.py
+++ src/views/routes.py
@@ -1,14 +1,29 @@
 from . import main
 from flask.views import MethodView
-from flask import make_response, jsonifycd 
-from src.controllers.generators import generate_candlesticks
+from flask import make_response, json
+import pandas as pd
 
 class ScreenerView(MethodView):
     """Class controlling public routes"""
     
     def get(self):
-        generate_candlesticks()
-        return make_response(jsonify({'msg':'success'}), 200)
+        files = pd.read_csv('src/datasets/symbols.csv', sep='\n', header=None)
+        dfs = []
+        
+        # iterate over the csv file to get symbol names
+        for symbol in files.iterrows():
+            symbol_name = symbol[1][0]
+            coin_json = cache.get('{}_file'.format(symbol_name))
+            
+            df = pd.read_json(coin_json, typ='series')
+            dfs.append(df)
+            
+        temp = pd.concat(dfs, axis=1)
+        res = {'data': temp}
+        res_json = json.dumps(res, default=lambda temp: json.loads(temp.to_json()))
+        results = json.loads(res_json)
+        # print(temp.transpose())
+        return make_response(results, 200)
     
     
 # define public routes
