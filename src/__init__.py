282b32fdf34c7dbf906d7a69aca201871f3709bc
loverofMush <tolujamal1@gmail.com>
2021-02-12 02:41:00 +0100

    - removed unused lines of code

--- src/__init__.py
+++ src/__init__.py
@@ -15,30 +15,6 @@ def createApp():
     app.config.from_object(Config)
     cache.init_app(app)
     
-    
-    # @app.route('/')
-    # def index():
-    #     generate_candlesticks()
-    #     # files = pd.read_csv('src/datasets/symbols.csv', sep='\n', header=None)
-    #     # dfs = []
-        
-    #     # # iterate over the csv file to get symbol names
-    #     # for symbol in files.iterrows():
-    #     #     symbol_name = symbol[1][0]
-    #     #     coin_json = cache.get('{}_file'.format(symbol_name))
-            
-    #     #     df = pd.read_json(coin_json, typ='series')
-    #     #     dfs.append(df)
-            
-    #     # temp = pd.concat(dfs, axis=1)
-    #     # res = {'data': temp}
-    #     # res_json = json.dumps(res, default=lambda temp: json.loads(temp.to_json()))
-    #     # results = json.loads(res_json)
-    #     # # print(temp.transpose())
-    #     # return make_response(results, 200)
-    #     return make_response(jsonify({'msg':'success'}), 200)
-    
-    
     with app.app_context():
         # add route blueprints
         from src.views import routes
