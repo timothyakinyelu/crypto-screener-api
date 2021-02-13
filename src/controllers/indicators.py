80890d7474eed6556e45282bf2d45dba8a282241
loverofMush <tolujamal1@gmail.com>
2021-02-13 15:10:37 +0100

    - changed function to use numpy instead of pandas

--- src/controllers/indicators.py
+++ src/controllers/indicators.py
@@ -1,46 +1,47 @@
 import pandas as pd
 import os
 import talib
+import numpy as np
 
 def run_indicators(symbol, file):
         try:
             if os.path.exists(file):
                 # read file
-                df = pd.read_csv(file, header=None)
+                df = np.genfromtxt(file, delimiter=',')
                 
                 # run talib indicators using dataframe
                 # create additional columns in the datafarame
-                openPrice = df[1]
-                highPrice = df[2]
-                lowPrice = df[3]
-                closePrice = df[4]
-                volume = df[5]
+                closePrice = df[:,4]
+                openPrice = df[:,1]
+                highPrice = df[:,2]
+                lowPrice = df[:,3]
+                volume = df[:,5]
                 
-                df[6] = talib.SMA(closePrice)
-                df[7] = talib.ATR(highPrice, lowPrice, closePrice)
-                df[8] = talib.NATR(highPrice, lowPrice, closePrice)
-                df[9] = talib.ADX(highPrice, lowPrice, closePrice)
-                df[10] = talib.MFI(highPrice, lowPrice, closePrice, volume)
-                df[11] = talib.PPO(closePrice, fastperiod=12, slowperiod=26, matype=0)
-                df[12] = talib.RSI(closePrice)
-                df[13], df[14] = talib.STOCH(highPrice, lowPrice, closePrice, fastk_period=14, slowk_period=3, slowk_matype=0, slowd_period=3, slowd_matype=0)
-                df[15], df[16], df[17] = talib.MACD(closePrice, fastperiod=12, slowperiod=26, signalperiod=9)
+                sma = talib.SMA(closePrice)
+                atr = talib.ATR(highPrice, lowPrice, closePrice)
+                natr = talib.NATR(highPrice, lowPrice, closePrice)
+                adx = talib.ADX(highPrice, lowPrice, closePrice)
+                mfi = talib.MFI(highPrice, lowPrice, closePrice, volume)
+                ppo = talib.PPO(closePrice, fastperiod=12, slowperiod=26, matype=0)
+                rsi = talib.RSI(closePrice)
+                slowk, slowd = talib.STOCH(highPrice, lowPrice, closePrice, fastk_period=14, slowk_period=3, slowd_period=3)
+                macd, macdsignal, macdhist = talib.MACD(closePrice, fastperiod=12, slowperiod=26, signalperiod=9)
             
                 # create crypto object
                 crypto = {}
                 
                 crypto['ticker'] = symbol
-                crypto['price'] = df.tail(1).iloc[0].get(4)
-                crypto['SMA14'] = df.tail(1).iloc[0].get(6)
-                crypto['ATR'] = df.tail(1).iloc[0].get(7)
-                crypto['NATR'] = df.tail(1).iloc[0].get(8)
-                crypto['ADX'] = df.tail(1).iloc[0].get(9)
-                crypto['MFI'] = df.tail(1).iloc[0].get(10)
-                crypto['RSI'] = df.tail(1).iloc[0].get(12)
-                crypto['STO'] = df.tail(1).iloc[0].get(13), df.tail(1).iloc[0].get(14)
-                crypto['MACD'] = df.tail(1).iloc[0].get(15)
+                crypto['price'] = closePrice[-1]
+                crypto['SMA14'] = sma[-1]
+                crypto['ATR'] = atr[-1]
+                crypto['NATR'] = natr[-1]
+                crypto['ADX'] = adx[-1]
+                crypto['MFI'] = mfi[-1]
+                crypto['RSI'] = rsi[-1]
+                crypto['STO'] = slowk[-1], slowd[-1]
+                crypto['MACD'] = macd[-1]
+                crypto['PPO'] = ppo[-1]
 
-                # res = {'data': crypto}
                 return crypto
             else:
                 pass
