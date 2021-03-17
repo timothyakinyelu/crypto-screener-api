242afda4252745f97dea8cfd160ec950025a494a
loverofMush <tolujamal1@gmail.com>
2021-03-17 23:13:08 +0100

    - updated wsgi file to include mulitprocessing

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,21 @@
 from src import createApp
 from src.controllers.generators import generate_candlesticks
-import click
+import pandas as pd
+import multiprocessing
 
 app = createApp()
 
 @app.cli.command()
-@click.argument("symbol")
-def scheduled(symbol):
-    generate_candlesticks(symbol)
+def scheduled():
+    pool = multiprocessing.Pool()
+    
+    df = pd.read_csv('src/datasets/symbols.csv')
+    for symbol in df.iterrows():
+        symbol_name = symbol[1][0]
+        
+        pool.apply_async(func=generate_candlesticks, args=(symbol_name,))
+    pool.close()
+    pool.join()
 
 if __name__ == '__main__':
     app.run()
